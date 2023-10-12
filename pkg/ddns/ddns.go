70d8a063a62564789976babeecb9ffaeb4e6c394
mteiting <m.teiting@deutsche-glasfaser.de>
2023-10-12 14:14:38 +0200

    adding support for multiple records

--- pkg/ddns/ddns.go
+++ pkg/ddns/ddns.go
@@ -2,6 +2,8 @@ package ddns
 
 import (
 	"fmt"
+	"log"
+	"matthias-kutz.com/hetzner-ddns/pkg/conf"
 
 	"matthias-kutz.com/hetzner-ddns/pkg/dns"
 	"matthias-kutz.com/hetzner-ddns/pkg/ip"
@@ -14,64 +16,83 @@ type Service struct {
 }
 
 type Parameter struct {
-	ZoneName   string
+	ZoneName string
+	Records  []Record
+}
+
+type Record struct {
 	RecordName string
 	RecordType string
 	TTL        int
+}
+
+func ConvertFromConfig(config conf.RecordConfig) []Record {
+	var recordList []Record
+
+	for _, recordConf := range config {
+		recordList = append(recordList, Record{
+			RecordName: recordConf.RecordName,
+			RecordType: recordConf.RecordType,
+			TTL:        recordConf.TTL,
+		})
+	}
+
+	return recordList
 }
 
 func (service Service) Run() {
 
 	// Check if online
 	if !service.IpProvider.IsOnline() {
-		fmt.Println("No connection to IP provider")
+		log.Println("No connection to IP provider")
 		return
 	}
 
 	// Request IP from ip provider
 	ip, err := service.IpProvider.Request()
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 
 	// Request zones from dns provider
 	zone, err := service.DnsProvider.RequestZone(service.Parameter.ZoneName)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 
-	// Request record from dns provider
-	record, err := service.DnsProvider.RequestRecord(zone, service.Parameter.RecordName, service.Parameter.RecordType)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	for _, dnsRecord := range service.Parameter.Records {
+		// Request record from dns provider
+		record, err := service.DnsProvider.RequestRecord(zone, dnsRecord.RecordName, dnsRecord.RecordType)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
 
-	// Create record with IP from ip provider to update record at the dns provider
-	updateRecord := dns.Record{
-		Id:     record.Id,
-		ZoneId: zone.Id,
-		Name:   service.Parameter.RecordName,
-		Type:   service.Parameter.RecordType,
-		TTL:    service.Parameter.TTL,
-		Value:  ip.Value,
-	}
+		// Create record with IP from ip provider to update record at the dns provider
+		updateRecord := dns.Record{
+			Id:     record.Id,
+			ZoneId: zone.Id,
+			Name:   dnsRecord.RecordName,
+			Type:   dnsRecord.RecordType,
+			TTL:    dnsRecord.TTL,
+			Value:  ip.Value,
+		}
 
-	// Check if record value has to be updated
-	if ip.Value == record.Value {
-		fmt.Println("No DNS update required for", service.Parameter.ZoneName, "with IP", ip.Value)
-		return
-	}
+		// Check if record value has to be updated
+		if ip.Value == record.Value {
+			//log.Printf("no update required for %s.%s [%s]\n", dnsRecord.RecordName, service.Parameter.ZoneName, ip.Value)
+			continue
+		}
 
-	fmt.Println("DNS update required for", service.Parameter.ZoneName, "with current IP", record.Value, "to IP", ip.Value)
-	// Update the record at the dns provider
-	updateRecord, err = service.DnsProvider.UpdateZoneRecord(zone, updateRecord)
-	if err != nil {
-		fmt.Println(err)
-		return
+		log.Printf("updating %s.%s [%s] to %s\n", dnsRecord.RecordName, service.Parameter.ZoneName, record.Value, ip.Value)
+		// Update the record at the dns provider
+		updateRecord, err = service.DnsProvider.UpdateZoneRecord(zone, updateRecord)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		log.Printf("updated %s.%s [%s]\n", dnsRecord.RecordName, service.Parameter.ZoneName, updateRecord.Value)
 	}
-	fmt.Println("Updated DNS for", service.Parameter.ZoneName, "from IP", record.Value, "to IP", updateRecord.Value)
-
 }
